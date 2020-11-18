2e752d572f77d3cdeeecaaa68bb3cc6ca7d8cfa4
Denis Dacenko <sined263@gmail.com>
2020-11-18 18:07:03 +0200

    2i

--- Auction/settings.py
+++ Auction/settings.py
@@ -139,9 +139,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
